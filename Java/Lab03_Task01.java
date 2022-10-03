0afc105f46b0c63728c462c62ce35ce5a6720fcb
MuhammadHaris033 <114726020+MuhammadHaris033@users.noreply.github.com>
2022-10-03 04:23:44 -0700

    Add files via upload

---
+++ Java/Lab03_Task01.java
@@ -0,0 +1,11 @@
+public class Lab03_Task01 {
+    public static void main(String[] args) {
+
+//        1.	Write a Java program to print out 1 to 50 numbers.
+
+
+        for (int i =1;i<=50;i++){
+            System.out.println(i);
+        }
+    }
+}
