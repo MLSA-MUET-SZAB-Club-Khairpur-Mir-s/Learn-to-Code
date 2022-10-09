021477d36b25c40c3773abe79f470b4b93aba6cd
jimmy-ali <115406703+jimmy-ali@users.noreply.github.com>
2022-10-09 20:59:01 +0500

    Update HelloWorld.cs

--- C#/HelloWorld.cs
+++ C#/HelloWorld.cs
@@ -4,7 +4,7 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!"); // Hello World Prints
         }
     }
 }
