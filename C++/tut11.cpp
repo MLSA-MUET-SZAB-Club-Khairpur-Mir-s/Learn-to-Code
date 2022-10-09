2390027f7e5f30480c5342fed5d4f19b2c535ed0
Unique Jain <uniquejn3004@gmail.com>
2022-10-09 22:11:46 +0530

    Add Some C++ snippet code

---
+++ C++/tut11.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     /* code */
+    //     if(i==2){
+    //         break;
+    //     }
+    //     cout<<i<<endl;
+    // }
+
+
+    for (int i = 0; i < 40; i++)
+    {
+        /* code */
+        if(i==2){
+            continue;
+        }
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
