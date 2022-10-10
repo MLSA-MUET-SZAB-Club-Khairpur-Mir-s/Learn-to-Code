dc7bd4602c02ab666640a9b7d4b419986dc96049
Shakeel135 <114926763+Shakeel135@users.noreply.github.com>
2022-10-10 16:31:48 +0500

    Created a game Rock Paper Scissors in Java

---
+++ Java/Game.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class Game {
+    public int number;
+    public int inputNo;
+    public int noOfGuesses = 0;
+
+    public int getNoOfGuesses() {
+        return noOfGuesses;
+    }
+
+    public void setNoOfGuesses(int noOfGuesses) {
+        noOfGuesses = noOfGuesses;
+    }
+
+    Game() {
+        Random ran = new Random();
+        this.number = ran.nextInt(100);
+    }
+
+    void takeUserInput() {
+        System.out.println("Guess the number");
+        Scanner sc = new Scanner(System.in);
+        inputNo = sc.nextInt();
+    }
+
+    boolean isCorrectNumber() {
+        noOfGuesses++;
+        if (inputNo == number) {
+            System.out.format("Yes, you guessed it right, it was %d\nYou guessed it %d attempts ", number,
+                    noOfGuesses);
+            return true;
+        } else if (inputNo < number) {
+            System.out.println("Too low...");
+        } else if (inputNo > number) {
+            System.out.println("Too high...");
+        }
+        return false;
+    }
+}
+
+public class Game1 {
+    public static void main(String[] args) {
+
+
+        Game g = new Game();
+        boolean b = false;
+        while (!b) {
+            g.takeUserInput();
+            b = g.isCorrectNumber();
+        }
+
+    }
+}
