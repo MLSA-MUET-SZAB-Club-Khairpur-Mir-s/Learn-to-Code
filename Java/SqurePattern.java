0d543a25648cb8a87988c1aae5dd6ab7a786ff64
hasnain12300 <114723972+hasnain12300@users.noreply.github.com>
2022-10-09 22:21:32 -0700

    Update SqurePattern.java

--- Java/SqurePattern.java
+++ Java/SqurePattern.java
@@ -3,33 +3,10 @@
 import java.io.*;
 
 class GFG {
-
-	static void print_rectangle(int k, int l)
-	{
-		int a;
-    int b;
-    
-		// Outer loop for rows
-		for (a = 1; a <= k; a++) {
-			// Inner loop for columns
-			for (b = 1; b <= l; b++) {
-				if (a == 1 || a == k || b == 1 || b == l)
-
-					System.out.print("*");
-				else
-
-					System.out.print(" ");
-			}
-			System.out.println();
-		}
-	}
-
+	
 	public static void main(String args[])
 	{
-		int rows = 8;
-    int columns = 22;
-
-		// Calling the method
-		print_rectangle(rows, columns);
+		  for(int i=0;i<5;i++)	  
+             System.out.println("***** ");
 	}
 }
