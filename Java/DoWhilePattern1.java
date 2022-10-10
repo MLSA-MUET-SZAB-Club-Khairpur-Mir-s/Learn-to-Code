11571834ba980369c193828eba4bb6796b86cc36
Muhammad Waleed <100406540+MuhammadWaleed110@users.noreply.github.com>
2022-10-10 18:12:56 +0500

    Pattern of  Rectangular Stars in java

---
+++ Java/DoWhilePattern1.java
@@ -0,0 +1,16 @@
+class DoWhilePattern1{
+public static void main(String args[]){
+int i=0;
+do{
+int j=1;
+while(j<=8)
+{
+System.out.print("*");
+j++;
+}
+System.out.println();
+i++;
+}
+while(i<=6);
+}
+}
