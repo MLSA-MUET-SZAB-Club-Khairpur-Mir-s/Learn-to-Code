298a56951d3bf3cdac294a0dc9ef93c8ab058ece
muskanalirizvi <98577455+muskanalirizvi@users.noreply.github.com>
2022-10-11 01:08:50 -0700

    Update Factorial.java

--- Java/Factorial.java
+++ Java/Factorial.java
@@ -1,10 +1,12 @@
 public class Factorial {
     public static void main(String[] args) {
-        int num = 6;
+        int num = 6;//Initializing a variable.
         long factorial = multiplyNumbers(num);
+        //Printing output.
         System.out.println("Factorial of " + num + " = " + factorial);
     }
     public static long multiplyNumbers(int num){
+        //Setting a condition.
         if (num >= 1)
             return num * multiplyNumbers(num - 1);
         else
