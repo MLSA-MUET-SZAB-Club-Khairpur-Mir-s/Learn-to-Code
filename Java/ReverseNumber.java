e4f4b1f4b4d15321f4adaad11356e9ceb95184c3
NadiaRajpoot <101450968+NadiaRajpoot@users.noreply.github.com>
2022-10-11 08:37:56 +0100

    Update ReverseNumber.java

--- Java/ReverseNumber.java
+++ Java/ReverseNumber.java
@@ -2,7 +2,8 @@ public class ReverseNumber {
     public static void main(String[] args) {
         int num = 987654321;
         int rem=0 , rev=0;
-        while(num!=0) {
+        //reversing anumber
+        while(num>0) {
             
                 rem = num % 10;
                 rev = rev * 10 + rem;
