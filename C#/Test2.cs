c464abcbbc975e9a6b85390836de05323ebcedaf
sub04 <115583338+sub04@users.noreply.github.com>
2022-10-11 23:36:47 +0500

    Create Test2.cs

---
+++ C#/Test2.cs
@@ -0,0 +1,25 @@
+// C# program to print a new line
+using System;
+using System.IO;
+using System.Text;
+
+namespace IncludeHelp
+{
+    class Test2
+    {
+        // Main Method 
+        static void Main(string[] args)
+        {
+            //using \n
+            Console.WriteLine("Hello\nWorld");
+            //using \x0A
+            Console.WriteLine("Hello\x0AWorld");
+            Console.WriteLine();
+
+            Console.WriteLine("end of the program");
+
+            //hit ENTER to exit the program
+            Console.ReadLine();
+        }
+    }
+}
