e0654f525f79b430541c609edaa9caa54a74dda0
Muskan Saleh <98576848+MuskanSaleh@users.noreply.github.com>
2022-10-11 12:35:37 +0500

    Update HelloWorld.cs

--- C#/HelloWorld.cs
+++ C#/HelloWorld.cs
@@ -4,7 +4,10 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!"); // Hello World Prints
+        
+        
+           System.Console.WriteLine("Hello World!");// Hello World Prints
+           System.Console.WriteLine("Microsoft excel is a surf excel to clean the computer...hehehe");
         }
-    }
+   }
 }
