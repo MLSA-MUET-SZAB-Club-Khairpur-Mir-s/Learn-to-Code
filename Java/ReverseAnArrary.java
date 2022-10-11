61ff8f40bd36e343c95efa19a2884c5947c5faac
SanjaiBhai <115535527+SanjaiBhai@users.noreply.github.com>
2022-10-11 13:18:35 +0500

    Create ReverseAnArrary.java

---
+++ Java/ReverseAnArrary.java
@@ -0,0 +1,9 @@
+public class ReverseAnArrary {
+    public static void main(String[] args) {
+        int a[]={9,8,7,6,5,4,3,2,1};
+        for (int i = a.length-1; i>=0;  i--){
+            System.out.print(a[i]);
+        }
+
+    }
+}
