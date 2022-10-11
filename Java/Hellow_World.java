9ce6cc1b32c52576aafdc55aa267f4291d0221eb
Muskan Saleh <98576848+MuskanSaleh@users.noreply.github.com>
2022-10-11 12:16:14 +0500

    Update Hellow_World.java

--- Java/Hellow_World.java
+++ Java/Hellow_World.java
@@ -1,5 +1,8 @@
 public class Hellow_World {
     public static void main(String arg[]) {
       System.out.println("Hellow world*****");
+         System.out.println("It is better to fail in originality than to succeed in Imitation");
+         System.out.println("is par zara goor kejeyega....!!!!");
+        
     }
 }
