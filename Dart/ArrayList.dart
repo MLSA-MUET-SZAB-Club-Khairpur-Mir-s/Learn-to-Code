623513d4ee32d5cb5aa689f2a9f02202f9bd2f0c
Muhammad Hasnain <98162005+hasnain-kk@users.noreply.github.com>
2022-10-11 19:40:03 +0500

    Add files via upload

---
+++ Dart/ArrayList.dart
@@ -0,0 +1,13 @@
+import 'dart:core';
+
+void main() {
+
+  //List of integers
+  List<int> intArr = [1,2,3,4,5];
+  print(intArr);
+  
+  //List of strings
+  List<String> strArr = ['hello', 'world'];
+  print(strArr);  
+  
+}
