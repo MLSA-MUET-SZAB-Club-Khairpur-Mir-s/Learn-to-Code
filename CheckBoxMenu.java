0b6d75d5ee90832e1c098c5f5accf0318e6b4b60
ChhotaElonMask <115547789+ChhotaElonMask@users.noreply.github.com>
2022-10-12 09:16:19 +0500

    Check Box Menu in Java

---
+++ CheckBoxMenu.java
@@ -0,0 +1,49 @@
+import java.awt.event.ActionEvent;  
+import java.awt.event.ActionListener;  
+import java.awt.event.KeyEvent;  
+import javax.swing.AbstractButton;  
+import javax.swing.Icon;  
+import javax.swing.JCheckBoxMenuItem;  
+import javax.swing.JFrame;  
+import javax.swing.JMenu;  
+import javax.swing.JMenuBar;  
+import javax.swing.JMenuItem;  
+  
+public class JavaCheckBoxMenu{  
+    public static void main(final String args[]){  
+        JFrame frame = new JFrame("Jmenu Example");  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        JMenuBar menuBar = new JMenuBar();  
+
+      JMenu fileMenu = new JMenu("File");  
+        fileMenu.setMnemonic(KeyEvent.VK_F);  
+        menuBar.add(fileMenu);  
+      
+      JMenuItem menuItem1 = new JMenuItem("Open", KeyEvent.VK_N);  
+        fileMenu.add(menuItem1);  
+  
+        JCheckBoxMenuItem caseMenuItem = new JCheckBoxMenuItem("Option_1");  
+        caseMenuItem.setMnemonic(KeyEvent.VK_C);  
+        fileMenu.add(caseMenuItem);  
+  
+        ActionListener aListener = new ActionListener() {  
+            public void actionPerformed(ActionEvent event) {  
+                AbstractButton aButton = (AbstractButton) event.getSource();  
+                boolean selected = aButton.getModel().isSelected();  
+                String newLabel;  
+                Icon newIcon;  
+                if (selected) {  
+                    newLabel = "Value-1";  
+                } else {  
+                    newLabel = "Value-2";  
+                }  
+                aButton.setText(newLabel);  
+            }  
+        };  
+  
+        caseMenuItem.addActionListener(aListener);  
+        frame.setJMenuBar(menuBar);  
+        frame.setSize(350, 250);  
+        frame.setVisible(true);  
+    }  
+}  
