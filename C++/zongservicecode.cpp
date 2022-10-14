642cbe0cb80935f523cb1daafc0385bdf819fc7c
RoseMiki <115810133+RoseMiki@users.noreply.github.com>
2022-10-14 21:11:02 +0500

    Create zongservicecode.cpp

---
+++ C++/zongservicecode.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,num,c ;
+    cout<<"Dial the code for services :";
+    cin>>a;
+    if (c=a){
+        cout<<"Thankyou for choosing zong network."<<endl;
+        cout<<"Dear customer please select your desired language for more details:"<<endl;
+    if(a=1){
+        cout<<"1"<<"."<<"English"<<endl;
+        cout<<"2"<<"."<<"Roman Urdu"<<endl;
+
+    }}
+    int press;
+    if( press=1){
+        cout<<"press 1 for English"<<endl;
+        cout<<"press 2 for Roman Urdu"<<endl;
+        }
+        cin>>press;
+    if(press==1){
+        cout<<"Dear Customer!Welcome to zong pakages."<<endl;
+        cout<<"World's best fastest and trusted network zong 4G"<<endl;
+        cout<<"Now zong customer have freedom to choose any package to contact with thier lovedones"<<endl;
+        cout<<"For call pakages please dial 333 for more details:"<<endl;
+        cout<<"For whatsapp plus offers dial 444 for more details:"<<endl;
+        cout<<"For internet packages dial 222 for more details:"<<endl;
+        int dial;
+        cin>>dial;
+        if(dial==333){
+            cout<<"....welcome to zong customer care ..."<<endl;
+            cout<<"zong offers a variety of call packages for its customers."<<endl;
+            cout<<"For subscribing these offers press:"<<endl;
+            cout<<"1.Monthly voice offer pkr: 300 "<<endl;
+            cout<<"2.Shandar haftawar offer pkr 120"<<endl;
+            cout<<"3.Daily voice offer pkr 70"<<endl;
+            cout<<"4.2 hour baqi pesa call offer 50"<<endl;
+        cin>>num;
+        if(num==1 || num==2 || num==3 || num==4)
+        {
+           cout<<"Your request is being proceed wait for the confirmation message"<<endl;}}
+
+
+        else if(dial==444){
+        cout<<"Kindly select any of following whatsapp bundles to subscribe: "<<endl;
+        cout<<"1. Whatsapp weekly offer pkr 100"<<endl;
+        cout<<"2. Whatsapp monthly offer pkr 200"<<endl;
+        cin>>num;
+        if(num==1 || num==2){
+            cout<<"Your request is being proceed wait for the confirmation message"<<endl;
+        }
+         }
+
+        if(dial==222){
+            cout<<"Kindly select any of following internet bundles to subscribe:"<<endl;
+            cout<<"1.Weekly internet bundle 5 GB pkr 150"<<endl;
+            cout<<"2.Monthly internet bundle 25 GB pkr 350"<<endl;
+        }
+
+         cin>>num;
+         if(num==1 || num==2){
+            cout<<"Your request is being proceed wait for the confirmation message"<<endl;
+         }
+
+
+        }
+    else if(press==2){
+        cout<<"Khushamdeed!zong pakages m"<<endl;
+        cout<<"Duniya ka teztareen or pur ehtmad network zong 4G."<<endl;
+        cout<<"zong k pakages se abhi fori tor pr faeda hasil kre or apno se rabta kre."<<endl;
+        cout<<"Call pakages hasil krne k liye teen teen teen(333) dabae "<<endl;
+        cout<<"Whatsapp plus offers hasil krne k liye chaar chaar chaar (444) dabae."<<endl;
+        cout<<"Or internet packages hasil krne k liye abhi do do do (222) dabae."<<endl;
+        int dial;
+        cin>>dial;
+        if(dial==333){
+                cout<<"....khushamdeed ap logon ko zong customer care m....."<<endl;
+            cout<<"zong laya apne customers k lye bhut sare or behtreen pakages..."<<endl;
+            cout<<"mundarja zeel offers mein se koii offer intekhab karein"<<endl;
+            cout<<"1.Monthly voice offer pkr:300"<<endl;
+            cout<<"2.Shandar haftawar offer pkr:120"<<endl;
+            cout<<"3.Daily voice offer pkr:70"<<endl;
+            cout<<"4.2 hour baqi pesa call offer pkr:50"<<endl;
+            cout<<"in offers ko abhi hasil kre"<<endl;
+
+        cin>>num;
+        if(num=1 || num==2 || num==3 ||num==4){
+
+           cout<<"Barai mehrbani intezar farmaein apko tasdeiqi paigham bhja ja rha hai"<<endl;
+        }
+
+        }
+        else if (dial==444){
+            cout<<"mundarja zeel offers mein se koii bhi offer intekhab karein"<<endl;
+            cout<<"1.haftawar whatsapp offer pkr 100"<<endl;
+            cout<<"2.Mahana whatsapp offer pkr 200"<<endl;
+
+        }
+
+           if(num==1 ||num==2){
+            cout<<"Barai mehrbani intezar farmaein apko tasdeiqi paigham bhja ja rha hai"<<endl;
+
+           }
+           else if(dial==222){
+            cout<<"mundarja zeel offers mein se koii bhi offer intekhab karein"<<endl;
+            cout<<"1.Haftawar internet package 5GB pkr: 150"<<endl;
+            cout<<"2.Mahana internet package 25GB pkr:  350"<<endl;
+           }
+
+           cin>>num;
+           if(num==1 ||num==2){
+            cout<<"Barai mehrbani intezar farmaein apko tasdeiqi paigham bhja ja rha hai"<<endl;
+
+
+
+
+    }}
+
+
+
+
+
+
+
+
+    return 0;
+}
