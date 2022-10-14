6aede2a720f7c78a072f15d3e4cb5c72e2bb3caf
Inzamam007 <104027043+Inzamam007@users.noreply.github.com>
2022-10-14 11:08:10 +0500

    Update Function.java

--- Java/Function.java
+++ Java/Function.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Function {
  //greateroftwonumbers
  static void greaterOfTwo(int a,int b){
+  //Checking Condition for the greater number
        if (a>b){
             System.out.print(a+" is greater.");
+        //Checking Condition for Less number
         }else if(a<b){
             System.out.print(b+" is greater.");
+        //If both condition don't match or are equal
         }else {
             System.out.println("both are equal.");
         }
