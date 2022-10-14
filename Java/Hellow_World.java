533c072d4729e2fd2d5f58548834d4c6cca44cf6
Awaiskhan69 <115722799+Awaiskhan69@users.noreply.github.com>
2022-10-14 08:41:24 +0500

    Update Hellow_World.java

--- Java/Hellow_World.java
+++ Java/Hellow_World.java
@@ -1,8 +1,7 @@
 public class Hellow_World {
     public static void main(String arg[]) {
 
-      for(int i=0; i<=1; i++){
-          System.out.print("hello world");
-
-    }
+     
+        System.out.println("hello world");
+}
 }
