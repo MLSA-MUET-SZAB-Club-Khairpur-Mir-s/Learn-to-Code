031cb5bc0ac080953f9087efffac8cbc0c75c376
Majid Ali <83289212+majidali55566@users.noreply.github.com>
2022-10-17 10:45:15 -0700

    Update AvgCalculator.java

--- Java/AvgCalculator.java
+++ Java/AvgCalculator.java
@@ -1,24 +1,24 @@
 
-import java.util.Scanner;
 public class AvgCalculator {
-    public static void main(String[]args){
-        // using Input 
-        Scanner sin = new Scanner (System.in);
-        byte s1 , s2 , s3;
-        System.out.println("Enter Your Marks in English");
-        s1=sin.nextByte();
-        System.out.println("Enter Your Marks in Urdu");
-        s2 = sin.nextByte();
-        System.out.println("Enter Your Marks in Programming");
-        s3=sin.nextByte();
-        float avg = (s1+s2+s3)/3.0f;
-        System.out.printf("Average is %f \n", avg);
-        if(avg>=40 && s1>=35 && s2>=35 && s3>=35){
-            System.out.print("You are Expert in These Subs");
-        }
-        // using condition
+    public static void main(String[]args){ 
+       AvergeCalculator avergeCalculator=new AvergeCalculator(45,56,76);
+       if(avergeCalculator.isExpert()){
+           System.out.println("You are expert in these fields");
         else{
-            System.out.print("Loser!");
-        }
+            System.out.println("Please try hard to atleast pass these subjects");
+    }
+}
+class AvergeCalculator{
+   private int marksInEnglish;
+   private int marksInUrdu;
+   private int markInProgramming;
+    AverageCalculator(int marksInEnglish,int marksInUrdu,int markInProgramming){
+        this.marksInEnglish=marksInEnglish;
+        this.marksInUrdu=marksInUrdu;
+        this.markInProgramming=markInProgramming;
+    }
+    public boolean isExpert(){
+        double average=(marksInEnglish+marksInUrdu+markInProgramming);
+        return (average>=40 && marksInEnglish>=35 && marksInUrdu>=35 && markInProgramming>=35);
     }
 }
