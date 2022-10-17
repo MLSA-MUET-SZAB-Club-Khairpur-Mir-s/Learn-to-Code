da6bdeea24b5681cb8433e457ac27cc674c6df68
awaisbaloch231 <116003228+awaisbaloch231@users.noreply.github.com>
2022-10-17 18:50:05 +0500

    Update Average.java

--- Java/Average.java
+++ Java/Average.java
@@ -1,12 +1,13 @@
 public class Average {
-
+// main methode
     public static void main(String[] args) {
         double[] numArray = { 45.3, 67.5, -45.6, 20.34, 33.0, 45.6 };
         double sum = 0.0;
-
+// loop
         for (double num: numArray) {
            sum += num;
         }
+        // using formula of average
 
         double average = sum / numArray.length;
         System.out.format("The average is: %.2f", average);
