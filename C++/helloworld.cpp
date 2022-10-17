3e064fa81165f704a222a450ebf99817701825d5
ohsin020 <115971603+ohsin020@users.noreply.github.com>
2022-10-16 23:02:44 -0700

    Update helloworld.cpp

--- C++/helloworld.cpp
+++ C++/helloworld.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main() {
-   // program to print hellow world message 
-   cout<< "Software Department" <<endl;
+   // program to print name of string 
+   cout<< "Eng Hasnain Ali malik" <<endl;
   // Gul Zaman
   return 0;
 }
