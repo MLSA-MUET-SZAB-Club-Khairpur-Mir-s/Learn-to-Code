2d33d3fe8540d8518ae91f73c51fcbc435fe95f9
AneesAbbasi <92270767+AneesAbbasi@users.noreply.github.com>
2022-10-17 00:02:31 -0700

    Create jol.cpp

---
+++ jol.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace  std;
+int main(){
+for(int a=1;a<=6;a++){
+	for(int b=a;b<=6;b++){
+		cout<<" ";
+	}
+	for(int c=6;c<=a;c++){
+	cout<<"*";
+}
+cout<<endl;
+}
+}
+
