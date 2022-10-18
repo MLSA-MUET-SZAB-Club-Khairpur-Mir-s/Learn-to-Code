7fd39ce3a87a57094532ae5025865da5f8583fa5
Harsh Rathore <91665121+Harshrathore994@users.noreply.github.com>
2022-10-19 02:21:47 +0530

    Add files via upload

---
+++ Test3.cpp
@@ -0,0 +1,17 @@
+// Sum program
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter num 1"<<"\n";
+    cin>>a;
+    cout<<"Enter num 2"<<"\n";
+    cin>>b;
+
+    cout<<"Sum is : "<<a+b;
+
+    return 0;
+}
