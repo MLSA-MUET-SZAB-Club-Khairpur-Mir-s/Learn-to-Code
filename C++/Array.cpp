fca638934e63ca3c05bd387de59c867b5c559fb6
awaisbaloch231 <116003228+awaisbaloch231@users.noreply.github.com>
2022-10-18 10:07:26 +0500

    Update Array.cpp

--- C++/Array.cpp
+++ C++/Array.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-	 int X[]=[7,8];
-	 a[0]=1;
-	 a[2]=3;
+	int a[2]={7,8};
+	a[0]=1;
+	a[1]=3;
 	
-	for(int i=0;i<91;i++){
-		cout<<x[i]<<endl;
+	for(int i=0;i<2;i++){
+		cout<<a[i]<<endl;
 	}
 }
