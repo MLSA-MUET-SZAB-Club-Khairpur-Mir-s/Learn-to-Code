109487e2e470ee2b24a36e0dbce82a09f5727c58
SafiullahKorai123 <116307115+SafiullahKorai123@users.noreply.github.com>
2022-10-21 00:16:31 +0500

    Create BaseDeriveInheritanceExample.java

---
+++ Java/BaseDeriveInheritanceExample.java
@@ -0,0 +1,24 @@
+
+class Base{
+    String name;
+    Base(){
+        System.out.println("I'm non parameterized constructor of Base Class");
+    }
+    Base(String name){
+        System.out.println("I'm parameterized constructor of Base Class");
+    }
+}
+class Derived extends Base{
+    Derived(){
+        System.out.println("I'm non parameterized constructor of Derived Class");
+    }
+    Derived(String name){
+        super(name);
+        System.out.println("I'm parameterized constructor of Derived Class");
+    }
+}
+public class BaseDeriveInheritanceExample {
+    public static void main(String[] args) {
+        Derived obj = new Derived("hi");
+    }
+}
