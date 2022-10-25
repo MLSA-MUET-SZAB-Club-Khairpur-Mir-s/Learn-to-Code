c76212d3f034e4342722309e194a4614e9f4024e
Inziibrohi <116475838+Inziibrohi@users.noreply.github.com>
2022-10-25 10:27:06 +0500

    Update Electricitybill.cpp

--- C++/Electricitybill.cpp
+++ C++/Electricitybill.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int main()
-{
+{  
+    //Declaring unit variables 
     int unit, unit2, unit3;
     cout<<"Enter the value of units"<<endl;
     cin>>unit;
+    //Initializing unit variable If units are above 100.
     unit2=unit-100;
+    //Initializing unit variable If units are above 200.
     unit3=unit-200;
     //using control structure according to less or more usage of units
     if(unit<=100 )
