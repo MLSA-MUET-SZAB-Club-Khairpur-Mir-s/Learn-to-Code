271dd2c4f404c78dbdcee23236150e1ff650b4f8
Jameel Ali <83970847+JameelAli703@users.noreply.github.com>
2023-10-04 13:44:01 +0500

    Update bubble_sort.dart

--- Dart/bubble_sort.dart
+++ Dart/bubble_sort.dart
@@ -10,7 +10,7 @@ bubbleSort(List<int> array) {
   for (int i = 0; i < n - 1; i++) {
     for (int j = 0; j < n - i - 1; j++) {
       if (array[j] > array[j + 1]) {
-        int temp = array[j]; // here is temporary array
+        int temp = array[j]; //
         array[j] = array[j + 1];
         array[j + 1] = temp;
       }
