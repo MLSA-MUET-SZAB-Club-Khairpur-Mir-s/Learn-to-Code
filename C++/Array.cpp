6d762cace54c947b72b432274b48a46f76c66eb5
afrazkori <147363905+afrazkori@users.noreply.github.com>
2023-10-08 21:21:04 -0700

    Update Array.cpp

--- C++/Array.cpp
+++ C++/Array.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-	// initialize variable
+
 	int a[2]={7,8};
 	a[0]=1;
 	a[1]=3;
-	//o
+
 	
 	for(int i=0;i<2;i++){
-		cout<<a[i]<<endl;
-//pk	}
+		cout<<a[i]<<endl;	}
 }
 //ok
