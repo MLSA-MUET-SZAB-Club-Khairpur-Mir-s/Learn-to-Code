cc19cf7b5885373754cb19a015c64d040360a248
Manovekumar <115986064+Manovekumar@users.noreply.github.com>
2023-10-09 09:12:40 +0500

    Update Find_GCD_using_for_loop.cpp
    I done some updates in code

--- C++/Find HCF/Find_GCD_using_for_loop.cpp
+++ C++/Find HCF/Find_GCD_using_for_loop.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 int main() {
-  int n1, n2, hcf;
+  int n1, n2, hcf1;
   cout << "Enter two numbers: ";
   cin >> n1 >> n2;
 
   // swapping variables n1 and n2 if n2 is greater than n1.
   if ( n2 > n1) {   
-    int temp = n2;
+    int temp1 = n2;
     n2 = n1;
-    n1 = temp;
+    n1 = temp1;
   }
     
   for (int i = 1; i <=  n2; ++i) {
     if (n1 % i == 0 && n2 % i ==0) {
-      hcf = i;
+      hcf1 = i;
     }
   }
 
-  cout << "HCF = " << hcf;
+  cout << "HCF = " << hcf1;
 
   return 0;
 }
