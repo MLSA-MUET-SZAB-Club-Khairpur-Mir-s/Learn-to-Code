513f4514ff112751a74e764bcc95e7efeed7c91b
Muhammad Saleem Chachar <88135749+Muhammad-Saleem-Ch@users.noreply.github.com>
2023-10-09 11:14:42 -0700

    Update Array.cpp

--- C++/Array.cpp
+++ C++/Array.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main(){
 	// initialize variable
 	int a[2]={7,8};
-	a[0]=1;
-	a[1]=3;
+	a[0]=8;
+	a[1]=9;
 	//o
+// change the array index value
 	
 	for(int i=0;i<2;i++){
 		cout<<a[i]<<endl;
