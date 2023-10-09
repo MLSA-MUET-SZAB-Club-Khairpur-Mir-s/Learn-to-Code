7c6a76a29066bfbb8e7847baea716a49cac9fa56
Muhammad Saleem <141945270+Muhammad-Saleem1Ch@users.noreply.github.com>
2023-10-09 12:41:06 -0700

    Update BorderLayout.java

--- Java/BorderLayout.java
+++ Java/BorderLayout.java
@@ -11,13 +11,12 @@ Border(){
     JButton b2 = new JButton("SOUTH");; // the button will be labeled as SOUTH  
     JButton b3 = new JButton("EAST");; // the button will be labeled as EAST  
     JButton b4 = new JButton("WEST");; // the button will be labeled as WEST  
-    JButton b5 = new JButton("CENTER");; // the button will be labeled as CENTER  
-        
+      
     f.add(b1, BorderLayout.NORTH); // b1 will be placed in the North Direction    
     f.add(b2, BorderLayout.SOUTH);  // b2 will be placed in the South Direction    
     f.add(b3, BorderLayout.EAST);  // b2 will be placed in the East Direction    
     f.add(b4, BorderLayout.WEST);  // b2 will be placed in the West Direction    
-    f.add(b5, BorderLayout.CENTER);  // b2 will be placed in the Center    
+      
         
     f.setSize(300, 300);    
     f.setVisible(true);    
