d35635387915c2755308ee8cd3a677d9c7376b45
rizwanali362 <146235358+rizwanali362@users.noreply.github.com>
2023-10-10 00:52:57 +0500

    Update Electricitybill.cpp

--- C++/Electricitybill.cpp
+++ C++/Electricitybill.cpp
@@ -6,21 +6,21 @@ int main()
 {  
     //Declaring unit variables 
     int unit, unit2, unit3;
-    cout<<"Enter the value of units"<<endl;
+    cout<<"Enter the units"<<endl;
     cin>>unit;
     //Initializing unit variable If units are above 100.
-    unit2=unit-100;
+    unit2=unit-200;
     //Initializing unit variable If units are above 200.
-    unit3=unit-200;
+    unit3=unit-300;
     //using control structure according to less or more usage of units
-    if(unit<=100 )
+    if(unit<=200 )
         cout<<"Your electricity bill is " << unit*4;
 
 
-       else if(unit<=200)
+       else if(unit<=300)
         cout<<"Your electricity bill is " << 400+unit2*6;
 
-       else if(unit>200)
+       else if(unit>300)
         cout<<"Your electricity bill is " << 1000 +unit3*8<<endl;
 
     return 0;
