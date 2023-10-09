a7d1c8b1062e4307ab36ee1036d89b1982c7a444
rizoooooo <115442506+rizoooooo@users.noreply.github.com>
2023-10-09 09:21:21 +0500

    Update ball.py

--- Pong Game/ball.py
+++ Pong Game/ball.py
@@ -6,22 +6,22 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("red")
         self.penup()
-        self.x_move = 10
-        self.y_move = 10
-        self.move_speed = 0.1
+        self.x_move = 20
+        self.y_move = 20
+        self.move_speed = 0.2
     def move(self):
         new_x = self.xcor()+ self.x_move
         new_y = self.ycor()+ self.y_move
         self.goto(new_x,new_y)
         
     def bounce_y(self):
-        self.y_move *= -1
+        self.y_move *= -2
     def bounce_x(self):
-        self.x_move *= -1
-        self.move_speed *= 0.9
+        self.x_move *= -2
+        self.move_speed *= 0.8
     def reset_position(self):
         self.goto(0,0)
-        self.move_speed = 0.1
+        self.move_speed = 0.2
         self.bounce_x()
         //hhh
         
