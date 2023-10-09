1e49f5d633de4a3a9c2458abcf22366ecf19ad4e
Rizwanrizo <115441767+Rizwanrizo@users.noreply.github.com>
2023-10-09 09:25:38 +0500

    Update ArrayList.dart

--- Dart/ArrayList.dart
+++ Dart/ArrayList.dart
@@ -4,11 +4,11 @@ import 'dart:core';
 void main() {
 
   //List of integers
-  List<int> intArr = [1,2,3,4,5];
+  List<int> intArr = [5,6,7,8,9];
   print(intArr);
   
   //List of strings
-  List<String> strArr = ['hello', 'world'];
+  List<String> strArr = ['hello', 'world!'];
   print(strArr);  
   
 }
