4296ff335d82095de94f25fe2889665a607efcad
Rizwanrizo <115441767+Rizwanrizo@users.noreply.github.com>
2023-10-10 00:15:31 +0500

    Update Average.java

--- Java/Average.java
+++ Java/Average.java
@@ -1,6 +1,6 @@
 public class Average {
     public static void main(String[] args) {
-        double[] numArray = { 45.3, 67.5, -45.6, 20.34, 33.0, 45.6 };
+        double[] numArray = { 45.4, 67.6, -45.4, 20.33, 33.1, 45.4 };
         double sum = 0.0;
         for (double num: numArray) {
            sum += num;
