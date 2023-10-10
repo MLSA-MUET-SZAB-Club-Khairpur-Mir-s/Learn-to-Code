9b6e4f98f6486c25930caa64a2587948463c0c0f
awaisali32 <114724407+awaisali32@users.noreply.github.com>
2023-10-10 11:02:54 +0500

    Update HelloWorld2.cs

--- C#/HelloWorld2.cs
+++ C#/HelloWorld2.cs
@@ -4,14 +4,14 @@ using System;
 class HelloWorld2 {
 	static void Main() {
 		//print text without inserting new line after the message
-		Console.Write("Hello World,");
+		Console.Write("Hello");
 		Console.Writeln("How are you?");
 		//print next line
 		Console.WriteLine();
 		//print text with new line after the message
-		Console.WriteLine("Hello World");
+		Console.WriteLine("Hello");
 		Console.WriteLine("How are you?");
 		//print new line using escape sequence just like C language
-		Console.WriteLine("Hello World\nHow are you?");
+		Console.WriteLine("Hello\nHow are you?");
 	}
 }
