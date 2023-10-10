63ea8bd88a9b7f8db733c4b6f3e30e828a123ce3
SarwarJanjhi <114724998+SarwarJanjhi@users.noreply.github.com>
2023-10-10 07:21:19 +0100

    Update ArrayElementsSum.java

--- Java/ArrayElementsSum.java
+++ Java/ArrayElementsSum.java
@@ -1,6 +1,6 @@
 public class ArrayElementsSum {
     public static void main(String[]args){
-        float [] arr = {1.1f, 1.2f, 1.3f, 1.4f, 1.5f};
+        float [] arr = {1.8f, 1.3f, 1.7f, 1.7f, 1.5f};
         float sum = 0; // 2.5 + 2 + 1.5
         for(float ely : arr){
             sum += ely;
