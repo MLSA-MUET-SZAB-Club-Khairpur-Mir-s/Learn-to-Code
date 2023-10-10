8cc25ffdd9bb4d588f48b25959e4c998a7c39825
Kalyansingh045 <85118009+Kalyansingh045@users.noreply.github.com>
2023-10-10 00:57:55 -0700

    Update Fibonacci_Series_up_to_n_number_of_terms.cpp
    ok

--- C++/Fibonacci_Series_up_to_n_number_of_terms.cpp
+++ C++/Fibonacci_Series_up_to_n_number_of_terms.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    int m, t1 = 0, t2 = 1, nextTerm = 0;
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
 
     cout << "Enter the number of terms: ";
-    cin >> m;
+    cin >> n;
 
     cout << "Fibonacci Series: ";
 
-    for (int i = 1; i <= m; ++i) {
+    for (int i = 1; i <= n; ++i) {
         // Prints the first two terms.
         if(i == 1) {
             cout << t1 << ", ";
