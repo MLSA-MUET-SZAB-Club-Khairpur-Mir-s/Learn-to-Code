de0e0b5d3b8364e44176cf4ec0c32456ec094e5b
awaisali32 <114724407+awaisali32@users.noreply.github.com>
2023-10-10 11:05:35 +0500

    Update HelloWorld.cs

--- C#/HelloWorld.cs
+++ C#/HelloWorld.cs
@@ -5,8 +5,8 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            System.Console.WriteLine("Hello!"); // Hello Prints
-            System.Console.WriteLine("Hello World 2"); // Hello World Prints
+            System.Console.WriteLine("Hello world"); // Hello world Prints
+            System.Console.WriteLine("Hello World test"); // Hello World Prints
 
         }
    }
