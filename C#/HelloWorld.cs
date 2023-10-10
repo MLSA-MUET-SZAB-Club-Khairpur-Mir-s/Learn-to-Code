0963153ee9a73f37fa19aeebe64e9d382081aad8
awais3332 <115443058+awais3332@users.noreply.github.com>
2023-10-10 11:32:32 +0500

    Update HelloWorld.cs

--- C#/HelloWorld.cs
+++ C#/HelloWorld.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
 
             System.Console.WriteLine("Hello!"); // Hello Prints
             System.Console.WriteLine("Hello World 2"); // Hello World Prints
-
+             System.Console.WriteLine("MUET-SZAB");
         }
    }
 }
