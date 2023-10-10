ca9e11726b27f70f7a3888165d2966877f4efbc2
Manovekumar <115986064+Manovekumar@users.noreply.github.com>
2023-10-10 11:08:35 +0500

    Update Array.cpp
    okkk

--- C++/Array.cpp
+++ C++/Array.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
 	// initialize variable
-	int a[2]={7,8};
-	a[0]=1;
-	a[1]=3;
+	int b[2]={7,8};
+	b[0]=1;
+	b[1]=3;
 	//o
 	
 	for(int c=0;c<2;c++){
-		cout<<a[c]<<endl;
+		cout<<b[c]<<endl;
 //pk	}
 }
 //ok
