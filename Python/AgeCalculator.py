2ab385353295a295560bc4570c83d9827ac757e2
awaisali32 <114724407+awaisali32@users.noreply.github.com>
2023-10-12 22:25:56 +0500

    Update AgeCalculator.py

--- Python/AgeCalculator.py
+++ Python/AgeCalculator.py
@@ -1,9 +1,9 @@
 def AgeCalculator(y, m ,d):
   import datetime #importing the date time module using python
-  #
+  # This is A agg calculator program
   today = datetime.datetime.now().date() #taking today’s date by using the datetime.now() method of the datetime module
   dob = datetime.date(y, m, )
   age = int((today-dob).days / 365)
   print("age = "+age)
 AgeCalculator(2001, 10, 1)
-//theek aa bas
+
