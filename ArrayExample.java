36442b348c3add6e46d65043dd847daebe5f7678
Muhammad Waleed Ahmad <115546135+MuhammadWaleed-0786@users.noreply.github.com>
2023-10-15 01:08:36 +0500

    Create ArrayExample.java
    Create ArrayExample.java

---
+++ ArrayExample.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
