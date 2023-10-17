d21a4ba13263361475210cfaa4e7b91de248d835
Muhammad Saleem Chachar <88135749+Muhammad-Saleem-Ch@users.noreply.github.com>
2023-10-16 23:09:31 -0700

    Update Find_Size_of_a_Variable.cpp

--- C++/Find_Size_of_a_Variable.cpp
+++ C++/Find_Size_of_a_Variable.cpp
@@ -7,6 +7,7 @@ int main()
     cout << "Size of int:: " << sizeof(int) << " bytes" << endl;
     cout << "Size of float:: " << sizeof(float) << " bytes" << endl;
     cout << "Size of double:: " << sizeof(double) << " bytes" << endl;
+      cout << "Size of double:: " << sizeof(string) << " bytes" << endl;
 
     return 0;
 }
