ce53a198ccad42fe7fc21470b8386bc67a022a8f
Ayush Kumawat <ayushkumawat2112@gmail.com>
2023-10-26 11:34:02 +0530

    Create Password_Simulator.py

---
+++ Data structures/Password_Simulator.py
@@ -0,0 +1,24 @@
+print("         ____        ___  ___   _    _   ___")
+print("|      ||     |     |    |   | | \  / | |   ")
+print("|  /\  ||--   |     |    |   | |  \/  | |-- ")
+print("|_/  \_||____ |___  |___ |___| |      | |___")
+print("!!! This is password incrypted !!!")
+print("   [____Verification Needed____]   ")
+a=str(input("Enter Your Name:-"))
+i=0
+while i<11:
+ c=int(input(" Password-"))
+ if c!=1234:
+    print("Incorrect Password")
+ elif c==1234:
+    print("||||||||||||||||")
+    print("||||||||||||||||")
+    print("Correct Password!!")
+    print("||||||||||||||||")
+    print("||||||||||||||||")
+    print("Details have been verified")
+    print("Welcome, Master")
+    print("                         ~ All Things are Designed by Ayush_Kumawat")
+    break
+ i=i+1
+ print()
